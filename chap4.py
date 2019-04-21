1acdb04d745ac9bd381d89bda05932e959e35ff5
BasitMaju <49819425+BasitMaju@users.noreply.github.com>
2019-04-21 16:11:51 +0500

    Create chap4.py

---
+++ chap4.py
@@ -0,0 +1,41 @@
+#token = int(input("enter the token num: "))
+#while token!=0:
+#    value = int(input("enter the number:"))
+#    tax = value*0.0065
+  #  print("PROPERTY TAX",tax)
+  #  token =int(input("enter the token num: "))
+ #   print("program end")
+#count = 0
+#for i in range (5):
+  #  number=int(input("enter the number"))
+ #   count=count+number
+#print("no of bugs",count)
+
+#for i in range(10,31,5):
+#    burn= i*4.2
+#    print(burn)
+
+#race = int(input("entet the no of round"))
+#for i in range (race):
+#    time=int(input("enter the lap time for each round"))
+#    total = time/race
+#    print(format (total,".2f"))
+#print(format (total,".2f"))
+#average = total /i
+#print("average",average)
+
+#speed = int(input("enter the speed"))
+#print("hour\tdistance")
+#hour = 0
+#for hour in range(1,4):
+#         distance=speed*hour
+#         print(hour,"\t",distance)
+#         speed = int(input("enter the distance"))
+#         hour = int(input("enter the no of hours"))    
+#         print(hour,"\t",distance)
+score = float(input("enter the numbers"))
+while score<0:
+    print("error get input again")
+    score = float(input("enter the numbers"))
+    print(score)
+    
