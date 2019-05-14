79c8fb3902b38f9fb63e0c90e61a2c278848a15a
luqmanmoiz <49813486+luqmanmoiz@users.noreply.github.com>
2019-05-14 20:23:05 +0500

    Delete Tkinter.py

--- Abdulbasit/Luqman Moiz/Tkinter.py
+++
@@ -1,7 +0,0 @@
-import tkinter as TK
-from tkinter import messagebox as Text
-root = TK
-T = Text(root,height =2.5,width=35)
-T.pack()
-T.insert(END,"click here \nTeditor version 2.0\n")
-mainloop()
