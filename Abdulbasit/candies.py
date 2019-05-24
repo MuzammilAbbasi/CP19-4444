8a6cb0fda95d1a572816c09446f816c9415dc914
BasitMaju <49819425+BasitMaju@users.noreply.github.com>
2019-05-24 11:05:13 +0500

    clearing

---
+++ Abdulbasit/candies.py
@@ -0,0 +1,9 @@
+No_of_grand_Childern=5
+No_of_grand_daughter=3
+No_of_grand_son= (No_of_grand_Childern-No_of_grand_daughter)
+candies=90
+receivers=(No_of_grand_daughter*2)+No_of_grand_son
+candiesperchild= candies/receivers
+print ("candies/No_of_grand_daughter",(candiesperchild*2))
+print ("candies/No_of_grand_sons",(candiesperchild*1))
+      
